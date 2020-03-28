b0e9d783eb1d560c14c093efeffa36348d7a5dea
Cláudio Santos <cljos@hotmail.com>
2020-03-28 20:21:22 -0300

    Recursos melhorados no Backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
+
 const ongController = require('./controllers/ongController')
 const incidentController = require('./controllers/incidentController')
 const profileController = require('./controllers/profileController')
@@ -8,12 +10,43 @@ const routes = express.Router()
 routes.post('/sessions', sessionController.create)
 
 routes.get('/ongs', ongController.index)
-routes.post('/ongs', ongController.create)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), ongController.create)
 
-routes.post('/incident', incidentController.create)
-routes.get('/incidents', incidentController.index)
-routes.delete('/incident/:id', incidentController.delete)
+routes.post('/incident', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required().email(),
+        value: Joi.number().required()
+    })
+}), incidentController.create)
 
-routes.get('/profile', profileController.index)
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), incidentController.index)
+
+routes.delete('/incident/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), incidentController.delete)
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), profileController.index)
 
 module.exports = routes
